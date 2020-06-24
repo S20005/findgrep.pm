5298802dfa69a3e4f372701ac0df8a6ef84f8cd6
Yuto Kinjo <s20005@std.it-college.ac.jp>
2020-06-24 16:23:10 +0900

    ファイルパターンの指定の増加

--- findgrep.sh
+++ findgrep.sh
@@ -4,6 +4,10 @@ pattern=$1
 directory=$2
 if [ -z "$directory" ]; then
        directory="."
-fi       
+fi
+
+if [ -z "name" ]; then
+	name=*'*'
+fi
 find "$directory" -type f -print0 | xargs -0 grep -nH "$pattern"
 
